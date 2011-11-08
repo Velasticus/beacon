b157ab832a835c1b3502f7af156020db54c2ce5d
Velasticus <Velasticus@gmail.com>
2011-11-07 19:37:30 -0500

    Correct prologue for test file

--- src/test/scala/com/regressiongaming/bukkit/plugins/beacon/BeaconFileActorTest.scala
+++ src/test/scala/com/regressiongaming/bukkit/plugins/beacon/BeaconFileActorTest.scala
@@ -1,30 +1,5 @@
-/*
- * Copyright 2001-2009 Artima, Inc.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package com.regressiongaming.bukkit.plugins.beacon
 
-/*
-ScalaTest facilitates different styles of testing by providing traits you can mix
-together to get the behavior and syntax you prefer.  A few examples are
-included here.  For more information, visit:
-
-http://www.scalatest.org/
-
-One way to use ScalaTest is to help make JUnit or TestNG tests more
-clear and concise. Here's an example:
-*/
 import org.junit.runner.RunWith
 import org.scalatest.Spec
 import org.scalatest.junit.JUnitRunner
